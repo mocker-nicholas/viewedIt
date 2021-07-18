db2c5707c17348a1bc0d67b0c04f107a44815d8c
Nick Orth <nicholas.r.orth@gmail.com>
2021-07-18 16:31:05 -0500

    add initial request for reddit posts

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-const exploreBtn = document.querySelector('#explore-btn');
+const exploreBtn = document.querySelector('#search-btn');
 const subreddit = document.querySelector('#subreddit');
 const sort = document.querySelector('#sort')
 const searchSection = document.querySelector('#search')
+const feed = document.querySelector('#feed')
 
 // Show an error
 const showError = (elmt, msg) => {
   let errorBox = document.createElement('div')
-  elmt.style.borderColor = 'var(--red-salsa)'
-  elmt.style.outline = 'var(--red-salsa)'
   errorBox.textContent = `${msg}`
   errorBox.classList.add('error')
-  searchSection.prepend(errorBox);
+  searchSection.appendChild(errorBox);
+
+  setTimeout(() => {
+    errorBox.remove();
+  }, 3000)
 }
 
+const search = (subreddit, sort) => {
+    return fetch(`https://www.reddit.com/r/${subreddit}/${sort}/.json?count=20&t=day`)
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      return data.data.children.map(data => data.data);
+    })
+    .catch(err => console.log(err));
+  }
+
 exploreBtn.addEventListener('click', (e) => {
-  subreddit.style.borderColor = null
-  subreddit.style.outline = null
   if(searchSection.children[0].classList.contains('error')){
     searchSection.children[0].remove();
   }
   if(!subreddit.value){
-    showError(subreddit, 'Please list a subreddit')
+    showError(subreddit, 'Please enter a subreddit')
+    return;
   }
+
+  search(subreddit.value, sort.value)
+  .then((results) => {
+    console.log(results)
+  })
 })
+

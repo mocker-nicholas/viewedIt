d641ba30afb87509fe8a06ea3c775057a14dceb7
Nick Orth <nicholas.r.orth@gmail.com>
2022-02-05 13:36:31 -0600

    Add landing page form initial styles, add route for user/register, add routes folder, add controllers folder

--- app.js
+++ app.js
@@ -19,9 +19,14 @@ const app = express();
 
 app.set("view-engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("landing.ejs");
+});
+
+app.post("/user/register", (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(3000, () => {

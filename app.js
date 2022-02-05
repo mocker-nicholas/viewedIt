4f7ba9612069bf7c8485bec7917a7a1da1ea10bb
Nick Orth <nicholas.r.orth@gmail.com>
2022-02-05 09:12:37 -0600

    Add environment variables and dotenv

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import path from 'path';
-import ejs from 'ejs';
-import mongoose from 'mongoose';
-import ejsMate from 'ejs-mate';
-import fetch from 'node-fetch';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import path from "path";
+import ejs from "ejs";
+import mongoose from "mongoose";
+import ejsMate from "ejs-mate";
+import fetch from "node-fetch";
 
 const app = express();
 (async function main() {
-  try{
-    await mongoose.connect('mongodb://localhost:27017/test');
-    console.log('Mongoose Connected')
-  } catch (e){
-    console.log(e)
+  try {
+    await mongoose.connect("mongodb://localhost:27017/test");
+    console.log("Mongoose Connected");
+  } catch (e) {
+    console.log(e);
   }
 })();
 
-app.set('view-engine', 'ejs')
-app.use(express.static('public'))
+console.log(process.env.REDDIT_APP, process.env.REDDIT_SECRET);
 
+app.set("view-engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
-
-
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(3000, () => {
-  console.log('Listing on Port 3000');
-})
+  console.log("Listing on Port 3000");
+});

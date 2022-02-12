4bc5bd24f57a44b9ff03e120a28153fcf722065e
Nick Orth <nicholas.r.orth@gmail.com>
2022-02-11 18:53:22 -0600

    Add in password hash, salt, and validation for registration and login

--- controllers/usercontrol.js
+++ controllers/usercontrol.js
@@ -1,10 +1,12 @@
 import User from "../models/user.js";
 import { catchAsync } from "../util/middleware.js";
+import bcrypt from "bcrypt";
 
 export const registerUser = catchAsync(async (req, res) => {
   const { username, password, email } = req.body;
+  const hash = await bcrypt.hash(password, 12);
   try {
-    const newUser = await new User({ username, email, password });
+    const newUser = await new User({ username, email, password: hash });
     if (newUser) {
       const registeredUser = await newUser.save();
       req.session.user_id = registeredUser._id;
@@ -12,9 +14,11 @@ export const registerUser = catchAsync(async (req, res) => {
       return res.redirect("login");
     }
   } catch (e) {
-    if (e.code === 11000)
+    if (e.code === 11000) {
       req.flash("error", `${username} is not an available username`);
-    return res.redirect("/");
+      return res.redirect("/");
+    }
+    next(e);
   }
 });
 
@@ -22,6 +26,11 @@ export const loginUser = catchAsync(async (req, res, next) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
   if (!user) {
+    req.flash("error", `Error: Username or Password is invalid`);
+    return res.redirect("login");
+  }
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
     req.flash("error", `Error: Username or Password is invalid`);
     return res.redirect("login");
   }
